b2d35f5b5209e6226802a0329cee4ddc66764a09
boss0703 <prinnykurtis002@gmail.com>
2021-01-26 14:54:59 +0900

    削除機能の途中まで実装

--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import ListForm
+from .models import List
+
+
+def index(request):
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Todoリストにアイテムを追加しました。')
+
+    all_items = List.objects.all
+    return render(request, 'todo/index.html', {'all_items': all_items})
+
+
+def about(request):
+    context = {'first_name': 'keita', 'last_name': 'kameoka'}
+    return render(request, 'todo/about.html', context)
+
+
+def delete(request):
+    item = List.object.get(pk=list_id)
+    item.delete()
+    messages.success(request, 'Todoリストのアイテムを削除しました。')
+    return redirect('home')
